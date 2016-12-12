499f903e4f1485e259e0a44a13740f38c05b0e9b
Andrew Short <andrewjshort@gmail.com>
2016-12-12 20:47:59 +1000

    Find catkin workspace

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,20 @@
+import * as pfs from "./promise-fs";
+import { dirname } from "path";
 import * as vscode from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+  const baseDir = await findCatkinWorkspace(vscode.workspace.rootPath);
+}
+
+/**
+ * Traverses up directories to find a catkin workspace.
+ */
+async function findCatkinWorkspace(dir: string): Promise<string> {
+  while (dir && dirname(dir) !== dir) {
+    if (await pfs.exists(`${dir}/.catkin_workspace`)) {
+      return dir;
+    }
+
+    dir = dirname(dir);
+  }
 }
--- src/promise-fs.ts
+++ src/promise-fs.ts
@@ -4,6 +4,10 @@ function call(fn: Function, ...args: any[]): Promise<any> {
   return new Promise((c, e) => fn(...args, (err, res) => err ? e(err) : c(res)));
 }
 
+export function exists(path: string): Promise<boolean> {
+  return new Promise(c => fs.exists(path, c));
+}
+
 export function readFile(filename: string, encoding: string): Promise<string>;
 export function readFile(filename: string): Promise<Buffer>;
 
