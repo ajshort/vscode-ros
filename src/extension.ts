400998a90b0a15cad2bdc2efc16dc60b628510b2
Andrew Short <andrewjshort@gmail.com>
2016-12-15 17:08:59 +1000

    Move function to utils file

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,6 @@
-import * as pfs from "./promise-fs";
-import { dirname } from "path";
+import * as utils from "./utils";
 import * as vscode from "vscode";
 
 export async function activate(context: vscode.ExtensionContext) {
-  const baseDir = await findCatkinWorkspace(vscode.workspace.rootPath);
-}
-
-/**
- * Traverses up directories to find a catkin workspace.
- */
-async function findCatkinWorkspace(dir: string): Promise<string> {
-  while (dir && dirname(dir) !== dir) {
-    if (await pfs.exists(`${dir}/.catkin_workspace`)) {
-      return dir;
-    }
-
-    dir = dirname(dir);
-  }
+  const baseDir = await utils.findCatkinWorkspace(vscode.workspace.rootPath);
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,18 @@
 import * as pfs from "./promise-fs";
+import { dirname } from "path";
+
+/**
+ * Traverses up directories to find a catkin workspace.
+ */
+export async function findCatkinWorkspace(dir: string): Promise<string> {
+  while (dir && dirname(dir) !== dir) {
+    if (await pfs.exists(`${dir}/.catkin_workspace`)) {
+      return dir;
+    }
+
+    dir = dirname(dir);
+  }
+}
 
 /**
  * Gets the names of installed distros.
