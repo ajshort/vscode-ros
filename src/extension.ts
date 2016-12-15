03868a5681091339857ee56c4bfc81480b75374f
Andrew Short <andrewjshort@gmail.com>
2016-12-15 17:31:03 +1000

    Source or configure ros env

--- src/extension.ts
+++ src/extension.ts
@@ -3,4 +3,40 @@ import * as vscode from "vscode";
 
 export async function activate(context: vscode.ExtensionContext) {
   const baseDir = await utils.findCatkinWorkspace(vscode.workspace.rootPath);
+
+  if (!baseDir) {
+    return;
+  }
+
+  setup();
+}
+
+/**
+ * Loads the ROS environment, and prompts the user to select a distro if required.
+ */
+async function setup() {
+  let env = {};
+
+  const config = utils.getConfig();
+  const distro = config.get("distro", "");
+
+  if (distro) {
+    try {
+      env = await utils.sourceSetupFile(`/opt/ros/${distro}/setup.bash`, {});
+    } catch (err) {
+      vscode.window.showErrorMessage(`Could not source the setup file for ROS distro "${distro}".`);
+      return;
+    }
+  } else if (typeof process.env.ROS_ROOT !== "undefined") {
+    env = process.env;
+  } else {
+    const message = "The ROS distro is not configured.";
+    const configure = "Configure";
+
+    if (await vscode.window.showErrorMessage(message, configure) === configure) {
+      config.update("distro", await vscode.window.showQuickPick(utils.getDistros()));
+    }
+
+    return;
+  }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import * as pfs from "./promise-fs";
+import * as cp from "child_process";
 import { dirname } from "path";
 import * as vscode from "vscode";
 
@@ -27,4 +28,27 @@ export async function findCatkinWorkspace(dir: string): Promise<string> {
  */
 export function getDistros(): Promise<string[]> {
   return pfs.readdir("/opt/ros");
+}
+
+/**
+ * Executes a setup file and returns the resulting env.
+ */
+export function sourceSetupFile(filename: string, env?: any): Promise<any> {
+  return new Promise((resolve, reject) => {
+    cp.exec(`bash -c "source '${filename}' && env"`, { env }, (err, out) => {
+      if (!err) {
+        resolve(out.split("\n").reduce((env, line) => {
+          const index = line.indexOf("=");
+
+          if (index !== -1) {
+            env[line.substr(0, index)] = line.substr(index + 1);
+          }
+
+          return env;
+        }, {}));
+      } else {
+        reject(err);
+      }
+    });
+  });
 }
